e7842696c7a608e6d993e8880019123b84e98f8f
guitessmann09 <tessmanng9@gmail.com>
2024-12-13 22:21:02 -0300

    feat: add Sidebar and SidebarButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Sidebar from './components/Sidebar.jsx'
+
 const App = () => {
-  return <h1 className="p-6 text-center text-red-500">Hello World</h1>
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  )
 }
 
 export default App
