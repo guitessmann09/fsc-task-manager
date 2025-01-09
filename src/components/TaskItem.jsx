9597ecfabc42feb52f0a0c382fbcd349d5284484
guitessmann09 <tessmanng9@gmail.com>
2025-01-09 16:14:46 -0300

    feat: add react router dom and initial TaskDetailsPage

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 
 import {
@@ -71,9 +72,12 @@ const TaksItem = ({ task, handleCheckboxClick, onDeleteSuccess }) => {
             <TrashIcon className="text-brand-text-gray" />
           )}
         </Button>
-        <a href="#" className="transition-all hover:opacity-75">
+        <Link
+          to={`/task/${task.id}`}
+          className="transition-all hover:opacity-75"
+        >
           <DetailsIcon />
-        </a>
+        </Link>
       </div>
     </div>
   )
---
+++ src/pages/task-details.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+const TaskDetailsPage = () => {
+  const { taskId } = useParams()
+  const [task, setTask] = useState()
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
+        method: 'GET',
+      })
+
+      const data = await response.json()
+      setTask(data)
+    }
+
+    fetchTasks()
+  }, [taskId])
+
+  return (
+    <div>
+      <p>{task?.title}</p>
+      <p>{task?.description}</p>
+    </div>
+  )
+}
+export default TaskDetailsPage
